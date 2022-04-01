6e496d3ff9482d079ab3707059dd4913494522d2
zhang jinwei <96387365+flouxetind1@users.noreply.github.com>
2022-04-01 06:09:18 +0000

    create structure date

---
+++ src/main.c
@@ -0,0 +1,29 @@
+/*main/c*/
+#include <stdio.h>
+
+/*Source file with structure
+*StudentData
+*/
+
+struct StudentData{
+char *stu_name;
+int stu_id;
+int stu_age;
+};
+int main()
+{
+  /*student is the variable of stucture StudentData*/
+  struct StudentData student;
+  /*Assing the values to StudentData*/
+  student.stu_name="ZhangJinwei";
+  student.stu_id=2023405;
+  student.stu_age=22;
+
+  /*Displaying the values of struct memeber*/
+  printf("Hello, %s",student.stu_name);
+  printf("\nStudent Id is: %d",student.stu_id);
+  printf("\nStudent Age is: %d",student.stu_age);
+  printf("\nGoodbye");
+  return 0;
+}
+
